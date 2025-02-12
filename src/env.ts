31c15ff8e52d6a3d01623d09f5412d5057c40433
m1nhyeok <basakgrape@gmail.com>
2025-02-13 02:00:53 +0900

    환경 변수 이름 수정

--- src/env.ts
+++ src/env.ts
@@ -2,15 +2,16 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env.local' });
 
+// 환경변수 이름 대소문자 수정
 if (
-  !process.env.username ||
-  !process.env.password ||
-  !process.env.AWS_ACCESS_KEY_ID ||
+  !process.env.USERNAME || 
+  !process.env.PASSWORD || 
+  !process.env.AWS_ACCESS_KEY_ID || 
   !process.env.AWS_SECRET_ACCESS_KEY
-)
-  throw new Error('Please provide the environment variables');
+) throw new Error('필수 환경변수가 설정되지 않았습니다.');
 
-const { username, password, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } =
-  process.env;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
 
 export { username, password, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY };
